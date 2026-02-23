4f147a964ea82637707eb9103e6e345d092c58ac
Yuktha Sri Nunna <23mh1a05n8@acoe.edu.in>
2026-02-23 22:38:58 +0530

    Added CseDemo class and unit tests

--- src/main/java/Github/New/App.java
+++ src/main/java/Github/New/App.java
@@ -5,7 +5,6 @@ package Github.New;
  */
 public class App {
     public static void main(String[] args) {
-    	CseDemo demo = new CseDemo();
-        System.out.println(demo.add(2, 3));
+        System.out.println("Hello World");
     }
 }
--- src/main/java/Github/New/CseDemo.java
+++ src/main/java/Github/New/CseDemo.java
@@ -1,9 +1,10 @@
 package Github.New;
 
-public class CseDemo1{
-	System.out.println("Hello world!");
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
-
 /*
 public class CseDemo {
 
